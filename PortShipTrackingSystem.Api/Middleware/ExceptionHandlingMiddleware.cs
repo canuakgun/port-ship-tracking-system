f2fae05da4e02515ccaa156b43362566e9e651be
Can Ulaş Akgün <canuakgun@gmail.com>
2026-07-27 09:39:22 +0300

    Add global exception handling middleware mapping custom exceptions to HTTP status codes

---
+++ PortShipTrackingSystem.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+namespace PortShipTrackingSystem.Api.Middleware;
+
+using System.Net;
+using System.Text.Json;
+using PortShipTrackingSystem.Core.Exceptions;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var statusCode = exception switch
+        {
+            NotFoundException => HttpStatusCode.NotFound,
+            ValidationException => HttpStatusCode.BadRequest,
+            ConflictException => HttpStatusCode.Conflict,
+            _ => HttpStatusCode.InternalServerError,
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+
+        var response = new { message = exception.Message };
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
--- PortShipTrackingSystem.Api/Program.cs
+++ PortShipTrackingSystem.Api/Program.cs
@@ -3,11 +3,11 @@ using PortShipTrackingSystem.Infrastructure.Data;
 using PortShipTrackingSystem.Core.Interfaces;
 using PortShipTrackingSystem.Infrastructure.Repositories;
 using PortShipTrackingSystem.Services;
+using PortShipTrackingSystem.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -39,7 +39,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
